18329491adc404f8b125dcf58b5acdddba28ba4b
bradwinters <bwbiostats@gmail.com>
2021-10-27 20:05:55 -0700

    tkinter tutorial

---
+++ gui1.py
@@ -0,0 +1,17 @@
+#Import tkinter library
+from tkinter import *
+
+#Create an instance of Tkinter frame or window
+# then Set the geometry of tkinter frame
+win= Tk()
+win.geometry("750x250")
+
+
+def callback():
+   Label(win, text="Hello World!", font=('Century 20 bold')).pack(pady=4)
+
+#Create a Label and a Button widget
+btn=Button(win, text="Press Enter", command= callback)
+btn.pack(ipadx=10)
+win.bind('<Return>',lambda event:callback())
+win.mainloop()
