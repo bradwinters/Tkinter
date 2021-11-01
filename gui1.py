10629541c20f90318bbc8652a82b225e7ee996db
bradwinters <bwbiostats@gmail.com>
2021-10-31 21:20:13 -0700

    commit gui version 2

--- gui1.py
+++ gui1.py
@@ -4,11 +4,11 @@ from tkinter import *
 #Create an instance of Tkinter frame or window
 # then Set the geometry of tkinter frame
 win= Tk()
-win.geometry("750x250")
+win.geometry("850x350")
 
 
 def callback():
-   Label(win, text="Hello World!", font=('Century 20 bold')).pack(pady=4)
+   Label(win, text="Hello World 2", font=('Century 20 bold')).pack(pady=4)
 
 #Create a Label and a Button widget
 btn=Button(win, text="Press Enter", command= callback)
