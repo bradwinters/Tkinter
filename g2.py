61d34efc93d3bf30e227820ed20a238a3aecb641
bradwinters <bwbiostats@gmail.com>
2021-11-04 19:47:45 -0700

    slight change to test token

--- g2.py
+++ g2.py
@@ -3,7 +3,7 @@ import tkinter as tk
 class HelloWorld(tk.Frame):
     def __init__(self, parent):
         super(HelloWorld, self).__init__(parent)
-        self.label = tk.Label(self, text="Hello, World!")
+        self.label = tk.Label(self, text="Hello, Universe!")
         self.label.pack(padx=20, pady=20)
 
 if __name__ == "__main__":
